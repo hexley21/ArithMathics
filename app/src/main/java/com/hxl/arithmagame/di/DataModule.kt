1783800f297e16554ac9220ee53df8c300ea820b
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-06-27 21:18:19 +0400

    fix Hilt dependencies

--- app/src/main/java/com/hxl/arithmagame/di/DataModule.kt
+++ app/src/main/java/com/hxl/arithmagame/di/DataModule.kt
@@ -1,17 +1,26 @@
 package com.hxl.arithmagame.di
 
+import android.content.Context
 import com.hxl.data.repository.PreferenceRepositoryImpl
 import com.hxl.data.storage.PreferenceStorage
+import com.hxl.data.storage.sharedprefs.SharedPreferenceStorage
 import com.hxl.domain.repository.PreferenceRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataModule {
+
+    @Provides
+    @Singleton
+    fun providePreferenceStorage(@ApplicationContext context: Context): PreferenceStorage {
+        return SharedPreferenceStorage(context)
+    }
 
     @Provides
     @Singleton
