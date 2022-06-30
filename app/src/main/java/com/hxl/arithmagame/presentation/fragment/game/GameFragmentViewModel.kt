535a4fe6f5bc9ff7179a9f32251c2e2c93780b84
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-06-30 19:11:00 +0400

    add custom question generation & custom preferences to app

--- app/src/main/java/com/hxl/arithmagame/presentation/fragment/game/GameFragmentViewModel.kt
+++ app/src/main/java/com/hxl/arithmagame/presentation/fragment/game/GameFragmentViewModel.kt
@@ -2,6 +2,7 @@ package com.hxl.arithmagame.presentation.fragment.game
 
 import androidx.lifecycle.ViewModel
 import com.hxl.domain.models.Question
+import com.hxl.domain.usecase.prefs.GetCustom
 import com.hxl.domain.usecase.prefs.GetMode
 import com.hxl.domain.usecase.questions.GetQuestion
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,11 +11,13 @@ import javax.inject.Inject
 @HiltViewModel
 class GameFragmentViewModel @Inject constructor(
     private val getQuestion: GetQuestion,
-    private val getMode: GetMode
+    private val getMode: GetMode,
+    private val getCustom: GetCustom,
 ) : ViewModel() {
     val quantity = when (getMode()) {
         1 -> GetQuestion.medium
         2 -> GetQuestion.hard
+        3 -> getCustom().levels
         else -> GetQuestion.easy
     }
 
@@ -22,31 +25,27 @@ class GameFragmentViewModel @Inject constructor(
         return when(getMode()){
             1 -> mediumQuestions()
             2 -> hardQuestions()
+            3 -> customQuestion()
             else -> easyQuestions()
         }
     }
 
     private fun easyQuestions(): Array<Question> {
-        val questionList = mutableListOf<Question>()
-        for (i in 0 until quantity) {
-            questionList.add(getQuestion.easy())
-        }
-        return questionList.toTypedArray()
+        return Array(quantity){getQuestion.easy()}
     }
 
     private fun mediumQuestions(): Array<Question> {
-        val questionList = mutableListOf<Question>()
-        for (i in 0 until quantity) {
-            questionList.add(getQuestion.medium())
-        }
-        return questionList.toTypedArray()
+        return Array(quantity){getQuestion.medium()}
     }
 
     private fun hardQuestions(): Array<Question> {
-        val questionList = mutableListOf<Question>()
-        for (i in 0 until quantity) {
-            questionList.add(getQuestion.hard())
-        }
-        return questionList.toTypedArray()
+        return Array(quantity){getQuestion.hard()}
+    }
+
+    private fun customQuestion(): Array<Question> {
+        val operations = getCustom().operations
+        val numberRange = getCustom().numberRange
+        val operators = getCustom().operators
+        return Array(quantity){getQuestion(operations, numberRange, operators)}
     }
 }
