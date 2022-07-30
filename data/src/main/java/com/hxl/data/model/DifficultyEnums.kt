dc2c4e4294cc94603f122cc45e381429165c0df2
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-07-30 18:51:34 +0400

    feat(difficulty-enum): remove unnecessary operator

--- data/src/main/java/com/hxl/data/model/DifficultyEnums.kt
+++ data/src/main/java/com/hxl/data/model/DifficultyEnums.kt
@@ -9,5 +9,5 @@ enum class DifficultyEnums(
     EASY(QuestionDifficulty(5, 3, 1..10, "+-", 120)),
     MEDIUM(QuestionDifficulty(5, 5, 1..30, "++++----**", 480)),
     HARD(QuestionDifficulty(5, 5, 1..100, "+++---*/", 900)),
-    CUSTOM(QuestionDifficulty(1, 1, 1..1, "++++---*", 60));
+    CUSTOM(QuestionDifficulty(1, 1, 1..1, "+-*/", 60));
 }
