ddb0940a1f0b0a97624740c350424596c0d595b1
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-01 01:32:52 +0400

    feat: update main-activity difficulty variables
    difficulty methods now are inside view-model & RxJava is used to support reactive approach

--- app/src/main/java/com/hxl/arithmathics/presentation/activity/MainActivityViewModel.kt
+++ app/src/main/java/com/hxl/arithmathics/presentation/activity/MainActivityViewModel.kt
@@ -1,7 +1,9 @@
 package com.hxl.arithmathics.presentation.activity
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.hxl.domain.models.Difficulty
+import com.hxl.data.model.DifficultyEnums
+import com.hxl.data.storage.room.LocalDatabase
 import com.hxl.domain.models.GameResult
 import com.hxl.domain.usecase.database.game_history.ReadGameHistory
 import com.hxl.domain.usecase.database.game_history.InsertGameHistory
@@ -10,6 +12,9 @@ import com.hxl.domain.usecase.prefs.GetWelcome
 import com.hxl.domain.usecase.database.difficulty.InsertDifficulty
 import com.hxl.domain.usecase.prefs.SaveWelcome
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.*
 import javax.inject.Inject
 
@@ -17,20 +22,47 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor(
     private val getWelcome: GetWelcome,
     private val saveWelcome: SaveWelcome,
-    private val readDifficulty: ReadDifficulty,
-    private val insertDifficulty: InsertDifficulty,
+    val readDifficulty: ReadDifficulty,
+    val insertDifficulty: InsertDifficulty,
     private val readGameHistory: ReadGameHistory,
     private val insertGameHistory: InsertGameHistory
-) : ViewModel(){
+) : ViewModel() {
+    private val disposable = CompositeDisposable()
+
     var welcome
         get() = getWelcome()
         set(value) = saveWelcome(value)
 
-    var difficulty: Difficulty
-        get() = readDifficulty()
-        set(value) = insertDifficulty(value)
-
     var gameHistory: Stack<GameResult>
         get() = readGameHistory()
         set(value) = insertGameHistory(value)
+
+    fun checkDifficulty() {
+        disposable.add(readDifficulty()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    Log.d(LocalDatabase.TAG, "Difficulty was read successfully")
+                    disposable.clear()
+                },
+                { createDifficulty() }
+            )
+        )
+    }
+
+    private fun createDifficulty() {
+        disposable.add(insertDifficulty(DifficultyEnums.CUSTOM.difficulty)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    Log.d(LocalDatabase.TAG, "First record created")
+                    disposable.clear()
+                },
+                { Log.e(LocalDatabase.TAG, "Couldn't create first record", it) }
+            )
+        )
+    }
+
 }
