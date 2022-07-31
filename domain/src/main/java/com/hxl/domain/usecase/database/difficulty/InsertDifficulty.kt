79d2f38ecc2fb6d7ad20d3913da800ed3ed0ee7b
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-01 01:29:01 +0400

    feat: adapt use-cases to new database

--- domain/src/main/java/com/hxl/domain/usecase/database/difficulty/InsertDifficulty.kt
+++ domain/src/main/java/com/hxl/domain/usecase/database/difficulty/InsertDifficulty.kt
@@ -2,9 +2,10 @@ package com.hxl.domain.usecase.database.difficulty
 
 import com.hxl.domain.models.Difficulty
 import com.hxl.domain.repository.DifficultyRepository
+import io.reactivex.rxjava3.core.Completable
 
 class InsertDifficulty(private val difficultyRepository: DifficultyRepository) {
-    operator fun invoke(difficulty: Difficulty) {
-        difficultyRepository.insertCustom(difficulty)
+    operator fun invoke(difficulty: Difficulty): Completable {
+        return difficultyRepository.insertDifficulty(difficulty)
     }
 }
--- domain/src/main/java/com/hxl/domain/usecase/database/difficulty/ReadDifficulty.kt
+++ domain/src/main/java/com/hxl/domain/usecase/database/difficulty/ReadDifficulty.kt
@@ -2,10 +2,11 @@ package com.hxl.domain.usecase.database.difficulty
 
 import com.hxl.domain.models.Difficulty
 import com.hxl.domain.repository.DifficultyRepository
+import io.reactivex.rxjava3.core.Single
 
 class ReadDifficulty(private val difficultyRepository: DifficultyRepository) {
 
-    operator fun invoke(): Difficulty {
-        return difficultyRepository.readCustom()
+    operator fun invoke(): Single<Difficulty> {
+        return difficultyRepository.readDifficulty()
     }
 }
