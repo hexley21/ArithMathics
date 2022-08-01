ea7dc29452e6f2b14a7b09f64ddb095f4a2136aa
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-01 16:03:43 +0400

    feat(result-fragment): update game-result insert approach
    Adapted to RxJava

--- app/src/main/java/com/hxl/arithmathics/presentation/fragment/results/ResultFragmentViewModel.kt
+++ app/src/main/java/com/hxl/arithmathics/presentation/fragment/results/ResultFragmentViewModel.kt
@@ -8,6 +8,7 @@ import com.hxl.domain.usecase.database.game_history.InsertGameHistory
 import com.hxl.domain.usecase.database.difficulty.ReadDifficulty
 import com.hxl.domain.usecase.prefs.GetMode
 import com.hxl.data.model.DifficultyEnums
+import com.hxl.domain.models.Difficulty
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlin.math.floor
@@ -17,32 +18,26 @@ import kotlin.math.log
 class ResultFragmentViewModel @Inject constructor(
     private val readGameHistory: ReadGameHistory,
     private val insertGameHistory: InsertGameHistory,
-    private val getMode: GetMode,
-    private val readDifficulty: ReadDifficulty
+    val getMode: GetMode,
+    val readDifficulty: ReadDifficulty
 ) : ViewModel() {
     lateinit var questions: Array<Question>
     lateinit var answers: Array<String>
     var corrects: Int = 0
     var time: Int = 0
 
-    fun compareAnswers(): Array<Boolean> {
+    fun compareAnswers(difficulty: Difficulty): Array<Boolean> {
         val correctArray = Array(questions.size) { questions[it].answer == answers[it] }
         corrects = correctArray.count { it }
-        saveGame()
+        saveGame(difficulty)
         return correctArray
     }
 
-    private fun saveGame() {
-        val questionEnum = when (getMode()) {
-            0 -> DifficultyEnums.EASY.difficulty
-            1 -> DifficultyEnums.MEDIUM.difficulty
-            2 -> DifficultyEnums.HARD.difficulty
-            else -> readDifficulty()
-        }
-        val levels = questionEnum.levels.toFloat()
-        val operations = questionEnum.operations
-        val range = questionEnum.numberRange
-        val operators = questionEnum.operators.count()
+    private fun saveGame(gameMode: Difficulty) {
+        val levels = gameMode.levels.toFloat()
+        val operations = gameMode.operations
+        val range = gameMode.numberRange
+        val operators = gameMode.operators.count()
         val stack = readGameHistory()
 
         val difficulty = floor(log(levels, 100f) * log((range.last - range.first).toFloat(), 2f) * (operators * operations) / 2).toInt()
