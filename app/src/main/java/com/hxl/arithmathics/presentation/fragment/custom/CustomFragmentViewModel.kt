b129c00e646abece7202825ef132bccc7ff65cdd
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-02 00:03:39 +0400

    feat(custom-fragment)!: add occurrence chance choose for custom game

--- app/src/main/java/com/hxl/arithmathics/presentation/fragment/custom/CustomFragmentViewModel.kt
+++ app/src/main/java/com/hxl/arithmathics/presentation/fragment/custom/CustomFragmentViewModel.kt
@@ -1,7 +1,6 @@
 package com.hxl.arithmathics.presentation.fragment.custom
 
 import androidx.lifecycle.ViewModel
-import com.hxl.domain.models.Difficulty
 import com.hxl.domain.usecase.database.difficulty.ReadDifficulty
 import com.hxl.domain.usecase.prefs.GetTimer
 import com.hxl.domain.usecase.database.difficulty.InsertDifficulty
