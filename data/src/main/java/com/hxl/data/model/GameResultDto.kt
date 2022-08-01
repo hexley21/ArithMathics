e06ade8fc6c46d01cc85c54a7c426134732f2219
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-01 17:04:16 +0400

    feat(game-history): add database entity && DTO
    DTO: Data Transfer Object.
    This class also acts as a table for the database

---
+++ data/src/main/java/com/hxl/data/model/GameResultDto.kt
@@ -0,0 +1,30 @@
+package com.hxl.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hxl.domain.models.GameResult
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "game_history")
+data class GameResultDto(
+    @NotNull
+    @PrimaryKey
+    @ColumnInfo(name = "date") val date: Long,
+    @ColumnInfo(name = "difficulty") val difficulty: Int,
+    @ColumnInfo(name = "levels") val levels: Int,
+    @ColumnInfo(name = "corrects") val corrects: Int,
+    @ColumnInfo(name = "time") val time: Int
+) {
+    companion object {
+        @JvmStatic
+        fun toGameResult(gameResult: GameResultDto): GameResult {
+            return GameResult(
+                gameResult.difficulty,
+                gameResult.levels,
+                gameResult.corrects,
+                gameResult.time
+            )
+        }
+    }
+}
