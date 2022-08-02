3107bdf705de7f2d1403348a5b431b26116aea2f
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-02 18:41:22 +0400

    refactor(custom-difficulty): move difficulty check to welcome-fragment

--- app/src/main/java/com/hxl/arithmathics/presentation/activity/MainActivityViewModel.kt
+++ app/src/main/java/com/hxl/arithmathics/presentation/activity/MainActivityViewModel.kt
@@ -1,55 +1,17 @@
 package com.hxl.arithmathics.presentation.activity
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.hxl.data.model.DifficultyEnums
-import com.hxl.data.storage.room.LocalDatabase
-import com.hxl.domain.usecase.database.difficulty.ReadDifficulty
 import com.hxl.domain.usecase.prefs.GetWelcome
-import com.hxl.domain.usecase.database.difficulty.InsertDifficulty
 import com.hxl.domain.usecase.prefs.SaveWelcome
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     private val getWelcome: GetWelcome,
-    private val saveWelcome: SaveWelcome,
-    val readDifficulty: ReadDifficulty,
-    val insertDifficulty: InsertDifficulty,
+    private val saveWelcome: SaveWelcome
 ) : ViewModel() {
-    private val disposable = CompositeDisposable()
-
     var welcome
         get() = getWelcome()
         set(value) = saveWelcome(value)
-
-    fun checkDifficulty() {
-        disposable.add(readDifficulty()
-            .subscribeOn(Schedulers.io())
-            .subscribe(
-                {
-                    Log.e(LocalDatabase.TAG, "$it was read successfully")
-                    disposable.clear()
-                },
-                { createDifficulty() }
-            )
-        )
-    }
-
-    private fun createDifficulty() {
-        disposable.add(insertDifficulty(DifficultyEnums.CUSTOM.difficulty)
-            .subscribeOn(Schedulers.io())
-            .subscribe(
-                {
-                    Log.e(LocalDatabase.TAG, "First record created")
-                    disposable.clear()
-                },
-                { Log.e(LocalDatabase.TAG, "Couldn't create first record", it) }
-            )
-        )
-    }
-
 }
