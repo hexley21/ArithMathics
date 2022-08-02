c971052239f972318393d9b68982cfa76380f98e
Hexley21 <A.Kupreishvili.21@gmail.com>
2022-08-02 16:52:41 +0400

    feat(difficulty-enum): change difficulty values

--- data/src/main/java/com/hxl/data/model/DifficultyEnums.kt
+++ data/src/main/java/com/hxl/data/model/DifficultyEnums.kt
@@ -7,7 +7,7 @@ enum class DifficultyEnums(
     val difficulty: Difficulty
 ) {
     EASY(Difficulty(5, 3, 1..10, "+-", 120)),
-    MEDIUM(Difficulty(5, 5, 1..30, "++++----**", 480)),
+    MEDIUM(Difficulty(5, 5, 1..30, "+++---*", 480)),
     HARD(Difficulty(5, 5, 1..100, "+++---*/", 900)),
-    CUSTOM(Difficulty(1, 1, 1..1, "+-*/", 60));
+    CUSTOM(Difficulty(1, 1, 1..20, "+-*/", 60));
 }
