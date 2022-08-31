ec16654a8ccd480f0beeab1d724c50ce253eb19f
hexley21 <a.kupreishvili.21@gmail.com>
2022-08-31 21:05:25 +0400

    feat(info): add content to info fragment

--- app/src/main/java/com/hxl/arithmathics/presentation/fragment/info/InfoFragment.kt
+++ app/src/main/java/com/hxl/arithmathics/presentation/fragment/info/InfoFragment.kt
@@ -1,13 +1,18 @@
 package com.hxl.arithmathics.presentation.fragment.info
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hxl.arithmathics.R
 import com.hxl.arithmathics.databinding.FragmentInfoBinding
+import java.util.*
 
-class InfoFragment: Fragment() {
+
+class InfoFragment : Fragment() {
     lateinit var binding: FragmentInfoBinding
 
     override fun onCreateView(
@@ -16,6 +21,32 @@ class InfoFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentInfoBinding.inflate(layoutInflater, container, false)
+
+        binding.year = Calendar.getInstance().get(Calendar.YEAR).toString()
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvEmail.setOnClickListener { openEmail() }
+        binding.tvGithub.setOnClickListener {
+            openWeb("https://" + requireContext().getString(R.string.github_page))
+        }
+        binding.tvPrivacyPolicy.setOnClickListener { openWeb(requireContext().getString(R.string.privacy_policy)) }
+    }
+
+    private fun openEmail() {
+        startActivity(
+            Intent(
+                Intent.ACTION_SENDTO,
+                Uri.parse("mailto:${requireContext().getString(R.string.developer)}")
+            )
+        )
+    }
+
+    private fun openWeb(url: String) {
+        requireActivity().startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+    }
+
 }
